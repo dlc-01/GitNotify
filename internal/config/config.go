571938b2bba53aff946928d8813a494ad55beb55
DLC <shabrov23.02@gmail.com>
2026-03-23 01:36:25 +0300

    feat: add kafka to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,8 +8,10 @@ import (
 )
 
 type Config struct {
+	Debug    bool
 	Bot      BotConfig
 	Postgres PostgresConfig
+	Kafka    KafkaConfig
 }
 
 type BotConfig struct {
@@ -24,21 +26,36 @@ type PostgresConfig struct {
 	DBName   string
 }
 
+type KafkaConfig struct {
+	Brokers []string
+}
+
 func Load(path string) (*Config, error) {
-	viper.SetConfigFile(path)
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
-		if os.IsNotExist(err) {
-			return nil, wrap(path, ErrNotFound)
+	viper.SetDefault("kafka.brokers", []string{"localhost:9092"})
+	viper.SetDefault("postgres.port", 5432)
+	viper.SetDefault("debug", false)
+
+	if path != "" {
+		viper.SetConfigFile(path)
+		if err := viper.ReadInConfig(); err != nil {
+			if os.IsNotExist(err) {
+				return nil, wrap(path, ErrNotFound)
+			}
+			return nil, wrap(path, ErrInvalid)
 		}
-		return nil, wrap(path, ErrInvalid)
 	}
 
 	cfg := &Config{}
 	if err := viper.Unmarshal(cfg); err != nil {
 		return nil, wrap(path, ErrUnmarshal)
 	}
+
+	if len(cfg.Kafka.Brokers) == 1 && strings.Contains(cfg.Kafka.Brokers[0], ",") {
+		cfg.Kafka.Brokers = strings.Split(cfg.Kafka.Brokers[0], ",")
+	}
+
 	return cfg, nil
 }
